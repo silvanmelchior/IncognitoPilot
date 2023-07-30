abe0bff1671ec52153577713810da802982ea3a1
Silvan Melchior <silvan.melchior@zuehlke.com>
2023-07-30 14:28:35 +0200

    favicon

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google" content="notranslate" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" />
       </head>
       <body className={inter.className + " h-screen"}>{children}</body>
     </html>
