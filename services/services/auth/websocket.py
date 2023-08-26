8af6f0711b61901596a20dd0802f2b00ddd7df96
Silvan Melchior <silvan.melchior@zuehlke.com>
2023-08-26 10:46:43 +0200

    logging and clickjacking protection

--- services/services/auth/websocket.py
+++ services/services/auth/websocket.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import WebSocket
 
 from services.auth import AUTH_TOKEN
@@ -13,10 +15,12 @@ def _verify_origin(origin: str) -> bool:
 
 async def verify_websocket(websocket: WebSocket):
     if not _verify_origin(websocket.headers["origin"]):
+        logging.warning("Websocket with invalid origin")
         return False
 
     token = await websocket.receive_text()
     if token != AUTH_TOKEN:
+        logging.warning("Websocket with invalid token")
         return False
 
     return True
